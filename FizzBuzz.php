6a9a82ebe321f0f975a615a4bae08325256cc7d8
yuzuki57 <taburisu47@gmail.com>
2019-07-09 22:37:59 +0900

    フォーマッターで整形後

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,13 +1,13 @@
 <?php
 
-for ($i=1; $i <= 100 ; $i++) { 
+for ($i=1; $i <= 100 ; $i++) {
     if ($i % 15 === 0) {
         echo "FizzBuzz\n";
-    }elseif ($i % 5 === 0) {
+    } elseif ($i % 5 === 0) {
         echo "Buzz\n";
-    }elseif ($i % 3 === 0) {
+    } elseif ($i % 3 === 0) {
         echo "Fizz\n";
-    }else {
+    } else {
         echo $i."\n";
     }
 }
--- sample02.php
+++ sample02.php
@@ -1,5 +1,5 @@
 <?php
-$input=explode(' ',trim(fgets(STDIN)));
+$input=explode(' ', trim(fgets(STDIN)));
 
 $height=intval($input[0]) / 100;
 $weight=intval($input[1]);
@@ -9,17 +9,15 @@ $bmi = floor(($weight / ($height ** 2)) * 10) / 10;
 echo 'BMI:'.$bmi;
 
 if ($bmi < 18.5) {
-	echo "\n判定：低体重";
-}elseif ($bmi >= 18.5 && $bmi < 25) {
-	echo "\n判定：普通体重";
-}elseif ($bmi >= 25 && $bmi < 30) {
-	echo "\n判定：肥満（１度）";
-}elseif ($bmi >= 30 && $bmi < 35) {
-	echo "\n判定：肥満（２度）";
-}elseif ($bmi >= 35 && $bmi < 40) {
-	echo "\n判定：肥満（３度）";
-}elseif ($bmi >= 40) {
-	echo "\n判定：肥満（４度）";
+    echo "\n判定：低体重";
+} elseif ($bmi >= 18.5 && $bmi < 25) {
+    echo "\n判定：普通体重";
+} elseif ($bmi >= 25 && $bmi < 30) {
+    echo "\n判定：肥満（１度）";
+} elseif ($bmi >= 30 && $bmi < 35) {
+    echo "\n判定：肥満（２度）";
+} elseif ($bmi >= 35 && $bmi < 40) {
+    echo "\n判定：肥満（３度）";
+} elseif ($bmi >= 40) {
+    echo "\n判定：肥満（４度）";
 }
-
-?>
